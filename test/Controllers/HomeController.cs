879a483d290c553ca6d61fb0ae85b5046f1740cb
Frostjaw <45036335+Frostjaw@users.noreply.github.com>
2020-10-10 13:02:23 +0700

    Initial commit

---
+++ test/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PassUsingModel()
+        {
+            Random random = new Random();
+            double firstNumber = random.Next(0, 10);
+            double secondNumber = random.Next(0, 10);
+            Dictionary<string, double> data = new Dictionary<string, double>();
+            data.Add("firstNumber", firstNumber);
+            data.Add("secondNumber", secondNumber);
+            data.Add("sum", firstNumber + secondNumber);
+            data.Add("sub", firstNumber - secondNumber);
+            data.Add("mult", firstNumber * secondNumber);
+            if (secondNumber != 0) data.Add("div", firstNumber / secondNumber);
+            //else throw new DivideByZeroException();
+            return View(data);
+        }
+
+        public IActionResult PassUsingViewData()
+        {
+            Random random = new Random();
+            double firstNumber = random.Next(0, 10);
+            double secondNumber = random.Next(0, 10);
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["sum"] = firstNumber + secondNumber;
+            ViewData["sub"] = firstNumber - secondNumber;
+            ViewData["mult"] = firstNumber * secondNumber;
+            if (secondNumber != 0) ViewData["div"] = firstNumber / secondNumber;
+            //else throw new DivideByZeroException();
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag()
+        {
+            Random random = new Random();
+            double firstNumber = random.Next(0, 10);
+            double secondNumber = random.Next(0, 10);
+            ViewBag.firstNumber = firstNumber;
+            ViewBag.secondNumber = secondNumber;
+            ViewBag.sum = firstNumber + secondNumber;
+            ViewBag.sub = firstNumber - secondNumber;
+            ViewBag.mult = firstNumber * secondNumber;
+            if (secondNumber != 0) ViewBag.div = firstNumber / secondNumber;
+            //else throw new DivideByZeroException();
+            return View();
+        }
+
+        public IActionResult AccessServiceDirectly()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ test/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace test
+{
+    public class CalcService
+    {
+        private readonly double firstNumber;
+        private readonly double secondNumber;
+
+        public CalcService()
+        {
+            Random random = new Random();
+            firstNumber = random.Next(0, 10);
+            secondNumber = random.Next(0, 10);
+        }
+        public double getFirstNumber()
+        {
+            return firstNumber;
+        }
+        public double getSecondNumber()
+        {
+            return secondNumber;
+        }
+        public double getSum()
+        {
+            return firstNumber + secondNumber;
+        }
+        public double getSub()
+        {
+            return firstNumber - secondNumber;
+        }
+        public double getMult()
+        {
+            return firstNumber * secondNumber;
+        }
+        public double getDiv()
+        {
+            if (secondNumber!=0) return firstNumber / secondNumber;
+            return Double.NaN;
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<CalcService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            //app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
